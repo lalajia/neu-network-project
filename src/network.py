8563f5a3818b2d071356b02de7ba9680f4fbec5b
Tianyi Zhang <admin@ztybigcat.me>
2023-11-11 17:24:19 -0800

    consider little indian on macOS

--- src/network.py
+++ src/network.py
@@ -1,7 +1,45 @@
 import socket
 import struct
 import random
+from sys import platform
 
+
+def pack_helper(ip_ver_ihl, ip_type_of_service, ip_total_length, ip_identification, ip_fragment_offset, ip_time_to_live,
+                ip_protocol, checksum, ip_saddr, ip_daddr):
+    # if the system is macOS, pack fragmentation offset as little indian
+    if platform == "darwin":
+        be_header_part1 = struct.pack(
+            "!BB",  # Big-endian format
+            ip_ver_ihl,  # B
+            ip_type_of_service,  # B
+        )
+        packed_ip_total_length = struct.pack("<H", ip_total_length)
+        packed_ip_identification = struct.pack("!H", ip_identification)
+        packed_ip_fragment_offset = struct.pack("<H", ip_fragment_offset)
+        be_header_part2 = struct.pack(
+            "!BBH4s4s",  # Big-endian format
+            ip_time_to_live,  # B
+            ip_protocol,  # B
+            checksum,
+            ip_saddr,
+            ip_daddr
+        )
+        return be_header_part1 + packed_ip_total_length + packed_ip_identification + packed_ip_fragment_offset + \
+            be_header_part2
+    else:
+        return struct.pack(
+            "!BBHHHBBH4s4s",
+            ip_ver_ihl,  # B
+            ip_type_of_service,  # B
+            ip_total_length,  # H
+            ip_identification,  # H
+            ip_fragment_offset,  # H
+            ip_time_to_live,
+            ip_protocol,
+            0,
+            ip_saddr,
+            ip_daddr,
+        )
 
 def create_ip_packet(ip_source, ip_dest, udp_segment):
     ################## IP header ###################
@@ -36,21 +74,10 @@ def create_ip_packet(ip_source, ip_dest, udp_segment):
     # B = 1 byte
     # H = 2 bytes
     # 4s = 4 bytes
-    ip_header_without_checksum = struct.pack(
-        "!BBHHHBBH4s4s",
-        ip_ver_ihl,  # B
-        ip_type_of_service,  # B
-        ip_total_length,  # H
-        ip_identification,  # H
-        ip_fragment_offset,  # H
-        ip_time_to_live,
-        ip_protocol,
-        0,
-        ip_saddr,
-        ip_daddr,
-    )
-    ip_header = struct.pack(
-        "!BBHHHBBH4s4s",
+    ip_header_without_checksum = pack_helper(ip_ver_ihl, ip_type_of_service, ip_total_length, ip_identification,
+                                             ip_fragment_offset, ip_time_to_live, ip_protocol, 0, ip_saddr,
+                                             ip_daddr)
+    ip_header = pack_helper(
         ip_ver_ihl,  # B
         ip_type_of_service,  # B
         ip_total_length,  # H
@@ -65,9 +92,26 @@ def create_ip_packet(ip_source, ip_dest, udp_segment):
     return ip_header + udp_segment
 
 
+
+def unpack_helper(ip_header):
+    if platform == "darwin":
+        part1 = struct.unpack("!BB", ip_header[:2])
+        # Unpack little-endian fields
+        part2 = struct.unpack("<H", ip_header[2:4])
+        # Unpack big-endian fields
+        part3 = struct.unpack("!H", ip_header[4:6])
+        # Unpack little-endian fields
+        part4 = struct.unpack("<H", ip_header[6:8])
+        # Unpack the rest of the big-endian fields
+        part5 = struct.unpack("!BBH", ip_header[8:])
+        ip_header = part1 + part2 + part3 + part4 + part5
+    else:
+        ip_header = struct.unpack("!BBHHHBBH4s4s", ip_header)
+    return ip_header
+
 def unpack_ip_packet(ip_packet):
     ip_header = ip_packet[:20]
-    ip_header = struct.unpack("!BBHHHBBH4s4s", ip_header)
+    ip_header = unpack_helper(ip_header)
     ip_version = ip_header[0] >> 4
     ip_header_length = (ip_header[0] & 0xF) * 4
     ip_ttl = ip_header[5]
@@ -75,4 +119,7 @@ def unpack_ip_packet(ip_packet):
     ip_source_address = socket.inet_ntoa(ip_header[8])
     ip_destination_address = socket.inet_ntoa(ip_header[9])
     udp_segment = ip_packet[ip_header_length:]
+    # chop off the 20 bytes loopback header on macOS if the packet is from the same machine
+    if platform == "darwin" and ip_source_address == ip_destination_address:
+        udp_segment = udp_segment[20:]
     return ip_version, ip_header_length, ip_ttl, ip_protocol, ip_source_address, ip_destination_address, udp_segment
--- src/transport.py
+++ src/transport.py
@@ -21,19 +21,11 @@ def create_udp_segment(payload, source_ip, source_port, dest_ip, dest_port):
 
 
 def unpack_udp_segment(udp_segment):
-    udp_header = udp_segment[20:28]
+    udp_header = udp_segment[:8]
     udp_header = struct.unpack("!HHHH", udp_header)
     udp_source_port = udp_header[0]
     udp_destination_port = udp_header[1]
     udp_length = udp_header[2]
     udp_checksum = udp_header[3]
-    payload = udp_segment[28:]
-
-    # debug message
-    # print("loopback header is: ", udp_segment[0:20])
-    # print("udp_header is: ", udp_header)
-    # print("udp_source_port is: ", udp_source_port)
-    # print("udp_destination_port is: ", udp_destination_port)
-    # print("payload is: ", payload)
-
+    payload = udp_segment[8:]
     return udp_source_port, udp_destination_port, udp_length, udp_checksum, payload
