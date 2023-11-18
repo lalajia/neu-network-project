dbfe5acc765683451fd3f794f3713fed09607aab
yonglan <qi.yon@northeastern.edu>
2023-11-18 11:21:46 -0800

    added the calculation of checksum and the check for checksum in tansport.py

--- src/transport.py
+++ src/transport.py
@@ -1,22 +1,70 @@
 import struct
 
+# the pseudo header is used to calculate the checksum of udp segment, the length is the length of total udp segment
+def upd_pseudo_header(source_ip, dest_ip, udp_length):
+    return struct.pack("!HHHH", source_ip, dest_ip, udp_length, 0)
 
-def udp_checksum(udp_datagram, source_ip, dest_ip, udp_total_length):
-    def one_s_complement(msg):
-        # TODO: implement one's complement
-        return 0
+# this function is used to calculate the checksum of udp segment in the sender side
+def udp_checksum_calc(udp_segment, source_ip, dest_ip):
+    # add the pseudo header
+    udp_length = len(udp_segment) 
+    udp_pseudo_header = upd_pseudo_header(source_ip, dest_ip, udp_length)
+    udp_segment_temp = udp_pseudo_header + udp_segment
+    # calculate the checksum
+    checksum = 0
+    for i in range(0, len(udp_segment_temp), 2):
+        word = (udp_segment_temp[i] << 8) + udp_segment_temp[i + 1]
+        checksum += word
+        checksum = (checksum & 0xffff) + (checksum >> 16)
+    checksum = ~checksum & 0xffff
+    return checksum
 
-    # TODO: implement the checksum
-    # calculate pseudo header,pass to one's complement together with udp_datagram
-    return 0
+
+
+# def udp_checksum(udp_datagram, source_ip, dest_ip, udp_total_length):
+#     # this function takes a list of integers and calculates the one's complement sum of those integers.
+#     def one_s_complement(msg):
+#         # TODO: implement one's complement
+#         total_sum = sum(msg)
+#         while total_sum >> 16:
+#             total_sum = (total_sum & 0xFFFF) + (total_sum >> 16)
+#         return total_sum & 0xFFFF # keep the lower 16 bits
+    
+#     # Pseudo header
+#     pseudo_header = [
+#         (source_ip >> 16) & 0xFFFF,  # Source IP high 16 bits
+#         source_ip & 0xFFFF,           # Source IP low 16 bits
+#         (dest_ip >> 16) & 0xFFFF,    # Destination IP high 16 bits
+#         dest_ip & 0xFFFF,             # Destination IP low 16 bits
+#         0,                            # Reserved (all zeros)
+#         17,                           # Protocol (17 for UDP)
+#         udp_total_length              # UDP length
+#     ]
+
+#     # Combine pseudo header and UDP datagram
+#     data = pseudo_header + udp_datagram
+
+#     # Calculate one's complement sum
+#     checksum = one_s_complement(data)
+
+#     # Return the one's complement of the sum as the UDP checksum
+#     return checksum ^ 0xFFFF
+
 
 
 def create_udp_segment(payload, source_ip, source_port, dest_ip, dest_port):
-    udp_length = len(payload) + 8
-    udp_psudoheader = struct.pack("!HHHH", source_port, dest_port, udp_length, 0)
-    udp_datagram = udp_psudoheader + payload
-    checksum = udp_checksum(udp_datagram, source_ip, dest_ip, udp_length)
-    udp_header = struct.pack("!HHHH", source_port, dest_port, udp_length, checksum)
+    udp_total_length = len(payload) + 8 # 8 bytes for udp header
+    # checksum = upd_checksum_sender(payload, source_ip, dest_ip, udp_length)
+    # udp_psudoheader = struct.pack("!HHHH", source_port, dest_port, udp_length, 0) #TODO need fix?
+    # udp_datagram = udp_psudoheader + payload
+    # checksum = udp_checksum(udp_datagram, source_ip, dest_ip, udp_length)
+    # create a udp datagram without checksum
+    udp_header_withoutchecksum = struct.pack("!HHHH", source_port, dest_port, udp_total_length, 0)
+    udp_segment_withoutchecksum = udp_header_withoutchecksum + payload
+    # calculate the checksum
+    udp_checksum = udp_checksum_calc(udp_segment_withoutchecksum, source_ip, dest_ip, udp_total_length)
+    # update the udp header with checksum
+    udp_header = struct.pack("!HHHH", source_port, dest_port, udp_total_length, udp_checksum)
     return udp_header + payload
 
 
@@ -29,3 +77,17 @@ def unpack_udp_segment(udp_segment):
     udp_checksum = udp_header[3]
     payload = udp_segment[8:]
     return udp_source_port, udp_destination_port, udp_length, udp_checksum, payload
+
+
+# this function is used to check the checksum of udp segment in the receiver side
+def check_checksum(checksum, udp_segment, source_ip, dest_ip):
+    # create a pseudo header
+    udp_length = len(udp_segment)
+    udp_pseudo_header = upd_pseudo_header(source_ip, dest_ip, udp_length)
+    udp_segment_temp = udp_pseudo_header + udp_segment
+    # calculate the checksum
+    checksum_received = udp_checksum_calc(udp_segment_temp, source_ip, dest_ip)
+    if checksum_received == checksum:
+        return True
+    else:
+        return False
